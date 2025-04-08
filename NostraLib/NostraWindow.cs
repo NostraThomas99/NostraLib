8997922504a4624db0f19f1a3ebac2f847be59b1
NostraThomas99 <34015422+NostraThomas99@users.noreply.github.com>
2025-04-07 20:10:41 -0500

    Init Commit

---
+++ NostraLib/NostraWindow.cs
@@ -0,0 +1,18 @@
+﻿using Dalamud.Interface.Windowing;
+using ImGuiNET;
+
+namespace NostraLib;
+
+public class NostraWindow : Window
+{
+    public NostraWindow(string name, ImGuiWindowFlags flags = ImGuiWindowFlags.None, bool forceMainWindow = false) : base(name, flags, forceMainWindow)
+    {
+        TitleBarButtons.Add(new TitleBarButtons().DonationButton);
+        TitleBarButtons.Add(new TitleBarButtons().DiscordButton);
+    }
+
+    public override void Draw()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ NostraLib/TitleBarButtons.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics;
+using Dalamud.Interface;
+using Dalamud.Interface.Utility;
+using Dalamud.Interface.Windowing;
+using ImGuiNET;
+
+namespace NostraLib;
+
+public class TitleBarButtons
+{
+    public Window.TitleBarButton DonationButton => new Window.TitleBarButton()
+    {
+        Icon = FontAwesomeIcon.Heart,
+        ShowTooltip = () =>
+        {
+            ImGui.BeginTooltip();
+            ImGui.Text("Support NostraThomas on Ko-fi");
+            ImGui.EndTooltip();
+        },
+        Priority = 0,
+        Click = _ =>
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo()
+                {
+                    FileName = "https://ko-fi.com/nostrathomas",
+                    UseShellExecute = true,
+                    Verb = String.Empty
+                });
+            }
+            catch
+            {
+                // ignored
+            }
+        },
+        AvailableClickthrough = true
+    };
+
+    public Window.TitleBarButton DiscordButton => new Window.TitleBarButton()
+    {
+        Icon = FontAwesomeIcon.At,
+        ShowTooltip = () =>
+        {
+            ImGui.BeginTooltip();
+            ImGui.Text("Join the NostraThomas Industries Discord for support and updates");
+            ImGui.EndTooltip();
+        },
+        Priority = 1,
+        Click = _ =>
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo()
+                {
+                    FileName = "https://discord.gg/56nXcvE7nX",
+                    UseShellExecute = true,
+                    Verb = String.Empty
+                });
+            }
+            catch
+            {
+                // ignored
+            }
+        },
+        AvailableClickthrough = true
+    };
+}
